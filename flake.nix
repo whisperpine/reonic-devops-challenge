09665ae574d406fb76f127b5fb08d2b6eb7c03a8
Yusong <yusonglai64@gmail.com>
2025-10-18 01:28:25 +0800

    chore: set up dev environment by nix flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A Nix-flake-based web development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system: f { pkgs = import inputs.nixpkgs { inherit system; }; }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              biome # linting js and ts
+              nodejs_20 # nodejs v20 LTS
+              just # just a command runner
+            ];
+            shellHook = ''
+              npm run install:all
+            '';
+          };
+        }
+      );
+    };
+}
