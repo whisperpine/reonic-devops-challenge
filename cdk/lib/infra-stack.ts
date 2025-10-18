ecca2702416de4fbc97903afae8bd154f9064cdc
Yusong <yusonglai64@gmail.com>
2025-10-18 19:42:32 +0800

    feat(infra): manage AWS Resource Group by cdk

--- cdk/lib/infra-stack.ts
+++ cdk/lib/infra-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { CfnOutput, Stack } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Repository } from "aws-cdk-lib/aws-ecr";
+import { CfnGroup } from "aws-cdk-lib/aws-resourcegroups";
 
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,9 +16,9 @@ export class InfraStack extends Stack {
       },
     });
 
-    // Create ecr repository.
-    const repo = new Repository(this, "ECRRepository", {
-      repositoryName: "reonic-devops-challenge", // Human-readable name
+    // ECR repository.
+    const repo = new Repository(this, "ReonicECRRepository", {
+      repositoryName: "reonic-devops-ecr", // Human-readable name
       imageScanOnPush: true, // Auto-scan for vulnerabilities
       removalPolicy: cdk.RemovalPolicy.DESTROY, // Clean up on destroy
       emptyOnDelete: true, // Delete images on stack destroy
@@ -27,6 +28,23 @@ export class InfraStack extends Stack {
     new CfnOutput(this, "ECRRepositoryUri", { // ECR URI?
       value: repo.repositoryUri,
       description: "Push images here!",
+    });
+
+    // Resource group.
+    new CfnGroup(this, "ReonicResourceGroup", {
+      name: "reonic-devops-resource-group",
+      description: "All MyApp Dev resources",
+      resourceQuery: {
+        type: "TAG_FILTERS_1_0",
+        query: {
+          tagFilters: [
+            { key: "Repository", values: ["reonic-devops-challenge"] },
+            { key: "Owner", values: ["Yusong Lai"] },
+            { key: "CreatedBy", values: ["CDK"] },
+            // { key: "Environment", values: ["Dev, Prod"] },
+          ],
+        },
+      },
     });
   }
 }
