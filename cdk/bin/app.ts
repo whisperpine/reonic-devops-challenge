dbd1298a95992a6ec97795a796ba605922a1d063
Yusong <yusonglai64@gmail.com>
2025-10-18 19:01:11 +0800

    feat(infra): manage AWS ECR by cdk

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
-import * as cdk from "aws-cdk-lib";
+import { App } from "aws-cdk-lib";
 import { InfraStack } from "../lib/infra-stack";
 
-const app = new cdk.App();
-new InfraStack(app, "ReonicDevOpsStack");
+const app = new App();
+new InfraStack(app, "ReonicDevOpsStack", {
+  tags: {
+    "Environment": "Dev",
+  },
+});
--- cdk/lib/infra-stack.ts
+++ cdk/lib/infra-stack.ts
@@ -1,10 +1,32 @@
 import * as cdk from "aws-cdk-lib";
+import { CfnOutput, Stack } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { Repository } from "aws-cdk-lib/aws-ecr";
 
-export class InfraStack extends cdk.Stack {
+export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, {
+      ...props,
+      tags: {
+        "Repository": "reonic-devops-challenge",
+        "Owner": "Yusong Lai",
+        "CreatedBy": "CDK",
+        ...props?.tags,
+      },
+    });
 
-    // Add your stack resources here
+    // Create ecr repository.
+    const repo = new Repository(this, "ECRRepository", {
+      repositoryName: "reonic-devops-challenge", // Human-readable name
+      imageScanOnPush: true, // Auto-scan for vulnerabilities
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Clean up on destroy
+      emptyOnDelete: true, // Delete images on stack destroy
+    });
+
+    // Output: repository uri (for docker push)
+    new CfnOutput(this, "ECRRepositoryUri", { // ECR URI?
+      value: repo.repositoryUri,
+      description: "Push images here!",
+    });
   }
 }
